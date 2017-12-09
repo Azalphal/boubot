73f661e42188344e7aa1298d669fbc3ae6f36f2b
Bouyou34 <bouyou-mael@live.fr>
2017-12-09 23:59:53 +0100

    added commands and handlers

--- commands/av.js
+++ commands/av.js
@@ -2,3 +2,15 @@ if (message.content.startsWith(prefix + "av")) {
     let target = message.mentions.users.size === 0 ? message.author : message.guild.member(message.mentions.users.first()).user;
     return message.channel.send(target.avatarURL);
   }
+
+exports.run = (client, message) => {
+  let target = message.mentions.users.size === 0 ? message.author : message.guild.member(message.mentions.users.first()).user;
+  message.channel.send(targer.avatarURL);
+}
+
+exports.conf = {
+  enabled: true,
+  guildOnly: true,
+  aliases: [avi, avatar],
+  permLevel: 0
+};
