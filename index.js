76d25b8b1465da4c7e3ad035171567c4962e5fdb
Bouyou34 <bouyou-mael@live.fr>
2017-12-09 22:47:53 +0100

    fixed prefix and client.login

--- index.js
+++ index.js
@@ -1,6 +1,22 @@
+// When hosting on glitch.com
+/*
+const http = require('http');
+const express = require('express');
+const app = express();
+app.get("/", (request, response) => {
+  console.log(Date.now() + " Ping Received");
+  response.sendStatus(200);
+});
+app.listen(process.env.PORT);
+setInterval(() => {
+  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
+}, 280000);
+*/
+
 const discord = require('discord.js');
 const client = new discord.Client();
-const settings = require('./settings.json');
+// When not hosting on glitch.com 
+// const settings = require('./settings.json');
 const fs = require('fs');
 
 client.on('ready', () => {
@@ -12,19 +28,20 @@ client.on('ready', () => {
       `)
 });
 
+const prefix ) ">";
 client.on("message", (message) => {
-  if (!message.content.startsWith(settings.prefix) || message.author.bot) return;
+  if (!message.content.startsWith(prefix) || message.author.bot) return;
 
-  if (message.content.startsWith(settings.prefix + "ping")) {
+  if (message.content.startsWith(prefix + "ping")) {
     message.channel.send(`Pong! \`${Date.now() - message.createdTimestamp} ms\``);
   }
 
-  if (message.content.startsWith(settings.prefix + "av")) {
+  if (message.content.startsWith(prefix + "av")) {
     let target = message.mentions.users.size === 0 ? message.author : message.guild.member(message.mentions.users.first()).user;
     return message.channel.send(target.avatarURL);
   }
 });
 
 
-
+//client.login(process.env.TOKEN)
 client.login(settings.token);
