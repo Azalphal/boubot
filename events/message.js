25aab6030dc1d3d0e9cb85509402c0de70ca6581
Bouyou34 <bouyou-mael@live.fr>
2017-12-10 02:14:24 +0100

    fixes

--- events/message.js
+++ events/message.js
@@ -2,8 +2,8 @@ const config = require('../config.json');
 module.exports = message => {
     let client = message.client;
     if (message.author.bot) return;
-    if (!message.content.startwith(prefix)) return;
-    let command = message.content.split(' ')[0].slice(prefix.length);
+    if (!message.content.startwith(config.prefix)) return;
+    let command = message.content.split(' ')[0].slice(config.prefix.length);
     let params = message.content.split(' ').slice(1);
     let perms = client.elevation(message);
     let cmd;
